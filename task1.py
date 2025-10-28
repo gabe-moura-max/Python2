0e3c0850371b19d9657123207417703c72a35d76
gabe-moura-max <mouragabrielnetwork@gmail.com>
2025-10-27 22:27:29 -0300

    por hoje é só criançada

---
+++ task1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o 1° valor: "))
+num2 = int(input("Digite o 2° valor: "))
+
+soma = num1+num2
+subtracao = num1-num2
+divisao = num1/num2
+multiplicacao = num1*num2
+
+print(f"\nsoma de {num1} + {num2} = {soma}")
+print(f"subtracao de {num1} + {num2} = {subtracao}")
+print(f"divisao de {num1} + {num2} = {divisao}")
+print(f"multiplicacao de {num1} + {num2} = {multiplicacao}")
